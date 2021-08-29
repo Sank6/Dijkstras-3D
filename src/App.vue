e1f223335a4be62134cf6224595169b63b0442b0
Sank6 <25014241+Sank6@users.noreply.github.com>
2021-08-29 12:28:04 +0100

    show whats happening

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,10 @@
     :orbit-ctrl="{ enableDamping: true }"
     resize="window"
   >
-    <Camera ref="camera" :position="{ z: 10 }" />
+    <Camera ref="camera" :position="{ z: -20 }" />
     <Scene ref="scene">
       <AmbientLight color="#ffffff" :intensity="0.5" />
-      <InstancedMesh ref="imesh" :count="100">
+      <InstancedMesh ref="imesh" :count="max">
         <SphereGeometry :radius="0.05" />
         <PhongMaterial />
       </InstancedMesh>
@@ -49,17 +49,19 @@ export default defineComponent({
     AmbientLight,
   },
   setup() {
+    let max = 500;
     return {
       coords: [] as number[][],
       started: false,
       imesh: null as typeof InstancedMesh.mesh | null,
       renderer: null as RendererPublicInterface | null,
       scene: null as typeof Scene.scene | null,
-      max: 100,
+      max,
       maxDist: 3,
-      minDistances: new Array(100).fill(Infinity),
-      minPath: new Array(100).fill([]),
+      minDistances: new Array(max).fill(Infinity),
+      minPath: new Array(max).fill([]) as number[][],
       visitedNodes: [] as number[],
+      lines: [] as any[],
     };
   },
   mounted() {
@@ -97,24 +99,29 @@ export default defineComponent({
       // dijkstra
       let solved = false;
       let node = 0;
-      let path: string[] = [];
+      let path: number[] = [];
       let total_distance = 0;
       while (solved === false) {
         await this.pause();
+        
+        this.clearPath();
+        this.drawPath(path);
+
         const neighbors = this.getNeighbors(node);
-        console.log(neighbors, node);
         for (let key in neighbors) {
           let dist = total_distance + neighbors[key];
           if (this.minDistances[parseInt(key)] > dist) {
             this.minDistances[parseInt(key)] = dist;
-            this.minPath[parseInt(key)] = [...path, String(node)];
+            this.minPath[parseInt(key)] = [...path, node];
           }
+          this.joinPoints(node, parseInt(key));
         }
         
         if (Object.keys(neighbors).includes("1")) {
-          this.drawPath(node, 1)
+          path.push(node);
+          path.push(1);
           solved = true;
-          this.solved(path, total_distance + neighbors["1"]);
+          this.solved(path, total_distance + neighbors[1]);
           break;
         }
 
@@ -125,27 +132,24 @@ export default defineComponent({
         let min_index = 0;
         for (let node_ in this.minDistances) {
           if (!this.visitedNodes.includes(parseInt(node_)) && this.minDistances[node_] < min) {
-            console.log("changed")
             min = this.minDistances[node_];
             min_index = parseInt(node_);
           }
         }
 
         node = min_index;
-        path = this.minPath[node].concat([String(node)]);
+        path = [...this.minPath[node]];
         total_distance = this.minDistances[node];
-
-        console.log(node, path, total_distance);
       }
     },
     getNeighbors(node: number) {
       type Neighbours = {
-        [key: string]: number
+        [key: number]: number
       }
       const neighbours: Neighbours = {};
-      for (let i in this.coords) {
+      for (let i = 0; i < this.coords.length; i++) {
         const dist = this.distance(this.coords[node], this.coords[i])
-        if (parseInt(i) !== node && dist < this.maxDist) neighbours[i] = dist;
+        if (i !== node && dist < this.maxDist) neighbours[i] = dist;
       }
       return neighbours;
     },
@@ -154,7 +158,7 @@ export default defineComponent({
         (a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2 + (a[2] - b[2]) ** 2
       );
     },
-    drawPath(pointA: number, pointB: number) {
+    joinPoints(pointA: number, pointB: number) {
       let points = [];
       if (![0, 1].includes(pointA)) {
         this.imesh?.setColorAt(
@@ -172,18 +176,43 @@ export default defineComponent({
       );
       const line = new Line(
         (new BufferGeometry() as any).setFromPoints(points),
-        new LineBasicMaterial({ color: 0xffffff })
+        new LineBasicMaterial({ color: 0x1f1f1f })
       );
+      this.lines.push(line);
       this.scene?.add(line);
     },
-    solved(path: string[], total_distance: number) {
-      this.drawPath(parseInt(path[path.length - 1]), parseInt(path[path.length - 2]));
+    clearPath() {
+      this.lines.forEach((line) => {
+        this.scene?.remove(line);
+      });
+      this.lines = [];
+    },
+    drawPath(path: number[]) {
+      let points = [];
+      for (let i = 0; i < path.length; i++) {
+        points.push(
+          new Vector3(...this.coords[path[i]])
+        );
+        if (![0, 1].includes(path[i])) {
+          this.imesh?.setColorAt(
+            path[i],
+            new Color(1, 1, 0)
+          );
+        }
+      }
+      this.imesh.instanceColor.needsUpdate = true;
+      const line = new Line(
+        (new BufferGeometry() as any).setFromPoints(points),
+        new LineBasicMaterial({ color: 0xffffff })
+      );
+      this.lines.push(line);
+      this.scene?.add(line);
+    },
+    solved(path: number[], total_distance: number) {
       this.imesh?.setColorAt(0, new Color(1, 0, 0));
       this.imesh?.setColorAt(1, new Color(0, 1, 0));
-      for (let i = 0; i < path.length - 1; i++) {
-        this.drawPath(parseInt(path[i]), parseInt(path[i + 1]));
-      }
-      //alert(`Solved in ${total_distance}.`);
+      this.clearPath();
+      this.drawPath(path);
     },
     rand() {
       return parseFloat((Math.random() * 10 - 5).toFixed(5));
